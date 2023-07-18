9bdc9d203b429981163cb0c6ac9b9f2d589dd753
sk ashfak <skashfak18@gmail.com>
2023-07-18 12:23:41 +0530

    Apj Atribute

---
+++ src/Apj/Kalat.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import apj from "./A._P._J._Abdul_Kalam.jpg"
+
+export default function Kalat() {
+  return (
+    <>
+      <div className="container">
+        <div className="top_section">
+          <div className="image_content">
+            <img src={apj} alt="apj" width='100px' />
+          </div>
+          <div className="text-Content">
+            <h1> A.P.J Abdul Kalam </h1>
+            <h4>1939-2015 </h4>
+          </div>
+        </div>
+
+        <div className="biography_section">
+          <div className="about">
+            <h1>Missile Man Of A.P.J Abdul Kalam</h1>
+            <p> <b> Avul Pakir Jainulabdeen Abdul Kalam was born on 15 October 1931, </b>to a Tamil Muslim family in the pilgrimage centre
+              of Rameswaram on Pamban Island, then in the Madras Presidency and now in the State of Tamil Nadu. His father
+              Jainulabdeen Marakayar was a boat owner and imam of a local mosque;[9] his mother Ashiamma was a housewife.
+              [10][11][12][13] His father owned a ferry that took Hindu pilgrims back and forth between Rameswaram and the
+              now uninhabited Dhanushkodi.[14][15] Kalam was the youngest of four brothers and one sister in his family.
+              [16][17][18] His ancestors had been wealthy Marakayar traders and landowners, with numerous properties and
+              large tracts of land. Marakayar are a Muslim ethnic group found in coastal Tamil Nadu and Sri Lanka who claim
+              descent from Arab traders and local women. The family business had involved trading groceries between the mainland
+              and the island and to and from Sri Lanka, as well as ferrying pilgrims between the mainland and Pamban. With the
+              opening of the Pamban Bridge to the mainland in 1914, however, the businesses failed and the family fortune and
+              properties were lost by the 1920s, apart from the ancestral home. The family was poverty-stricken by the time
+              Kalam was born. As a young boy he had to sell newspapers to add to the family's meager income.</p>
+              </div>
+              <div className="book">
+                <h1>Bok Name Of A.P.J Abdul Kalam</h1>
+                <ul>
+                  <li>India 2020: A Vision for the New Millennium Publishing year: 1998</li>
+                  <li>Wings of Fire: An Autobiography Publishing year: 1999</li>
+                  <li> Ignited Minds: Unleashing the Power within India Publishing year: 2002</li>
+                  <li>The Luminous Sparks: A Biography in Verse and Colours Publishing year: 2004</li>
+                  <li>Guiding Souls: Dialogues on the Purpose of Life Publishing year: 2005</li>
+                  <li>Mission of India: A Vision of Indian Youth Publishing year: 2005</li>
+                  <li>Inspiring Thoughts: Quotation Series Publishing year: 2007</li>
+                  <li>You Are Born to Blossom: Take My Journey Beyond Publishing year: 2011</li>
+                  <li>The Scientific India: A Twenty First Century Guide to the World around Us Publishing year: 2011</li>
+                  <li>Failure to Success: Legendry Lives Publishing year: 2011</li>
+                  <li>Target 3 Billion Publishing year: 2011</li>
+                </ul>
+              </div>
+          </div>
+
+
+      <footer>
+        <p> Learn more A.P.J Abdul Kalam Go Now Wikipedia</p>
+      </footer>
+         
+      </div>
+
+
+
+
+
+
+    </>
+  )
+}
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Kalat from "./Apj/Kalat"
+import "./Apj/Abdul.css"
+
+
+
+
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <Kalat/>
+    </div>
+
+  )
+}
+
+
+
+export default App;
